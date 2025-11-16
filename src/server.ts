7d23156713b6f14711960ffb994714bb67fdb1f1
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-11-16 18:57:04 +0200

    Add src/server.ts file and insert code

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './graphql/schema.js';
+import { resolvers } from './graphql/resolvers.js';
+
+async function start() {
+  const app = express();
+  app.use(cors());
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+start();

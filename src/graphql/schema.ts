e834625a6a8b8070da2b7e7898004a05dc2b32a2
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-11-19 09:55:55 +0200

    update output per day ranking

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -3,16 +3,47 @@ import { gql } from 'apollo-server-express';
 export const typeDefs = gql`
   type Query {
     citySuggestions(name: String!): [City!]!
-    weatherForecast(lat: Float!, lng: Float!, days: Int = 7): WeatherForecast!
-    rankedActivities(lat: Float!, lng: Float!, days: Int = 7): [Activity!]!
+    weatherForecast(latitude: Float!, longitude: Float!, days: Int = 7): WeatherForecast!
+    rankedActivities(latitude: Float!, longitude: Float!, days: Int = 7): [RankedDay!]!
   }
 
-  type City { name: String! country: String! lat: Float! lng: Float! }
-  type DailyWeather { date: String! tempMax: Float! precipitationSum: Float! snowfallSum: Float! }
-  type WeatherForecast { daily: [DailyWeather!]! }
-  type Activity { name: ActivityName! rank: Int! suitabilityScore: Float! }
+  type City {
+    name: String!
+    country: String!
+    latitude: Float!
+    longitude: Float!
+  }
 
-  enum ActivityName { SKIING SURFING INDOOR_SIGHTSEEING OUTDOOR_SIGHTSEEING }
+  type DailyWeather {
+    date: String!
+    temperatureMax: Float!
+    temperatureMin: Float!
+    precipitation: Float!
+    windSpeed: Float!
+    snow: Float!
+  }
+
+  type WeatherForecast {
+    daily: [DailyWeather!]!
+  }
+
+  type Activity {
+    name: ActivityName!
+    rank: Int!
+    suitabilityScore: Float!
+  }
+
+  type RankedDay {
+    date: String!
+    activities: [Activity!]!
+  }
+
+  enum ActivityName {
+    SKIING
+    SURFING
+    INDOOR_SIGHTSEEING
+    OUTDOOR_SIGHTSEEING
+  }
 `;
 
 export enum ActivityName {
